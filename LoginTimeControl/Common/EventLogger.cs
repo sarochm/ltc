5d987dbbfac4d4028b54257d3cb4451270062812
Miroslav Saroch <saroch@seznam.cz>
2015-10-04 12:06:17 +0200

    added event logger

---
+++ LoginTimeControl/Common/EventLogger.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace Common
+{
+    public class EventLogger
+    {
+        private readonly EventLog _eventLog;
+
+        public EventLogger(string appName)
+        {
+            _eventLog = new EventLog {Source = appName};
+        }
+
+        public void Info(string message)
+        {
+            _eventLog.WriteEntry(message,EventLogEntryType.Information);
+        }
+
+        public void Warning(string message)
+        {
+            _eventLog.WriteEntry(message, EventLogEntryType.Warning);
+        }
+
+        public void Error(string message)
+        {
+            _eventLog.WriteEntry(message, EventLogEntryType.Error);
+        }
+    }
+}
---
+++ LoginTimeControl/Tests/EventLoggerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class EventLoggerTests
+    {
+        [TestMethod]
+        public void WritingToEventLog()
+        {
+            EventLogger eventLogger = new EventLogger("LoginTimeControlTest"); 
+
+            eventLogger.Info("Test info message");
+            eventLogger.Warning("Test warning message");
+            eventLogger.Error("Test error message");
+            
+            // assert - manual check in Windows Logs - Application
+        }
+    }
+}
