618b849015c4ca7c8e2ee396ac7677631c0ca3ea
Miroslav Saroch <saroch@seznam.cz>
2015-11-03 20:39:53 +0100

    try notificator implementation

--- LoginTimeControl/TryApp/Notificator.cs
+++ LoginTimeControl/TryApp/Notificator.cs
@@ -1,26 +1,45 @@
-﻿using Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
+using System.Timers;
 using System.Windows.Forms;
+using Common;
+using Timer = System.Timers.Timer;
 
 namespace TryApp
 {
     public class Notificator
     {
-        IEventLogger _eventLogger;
-        ISettingsManager _settingsManager;
-        NotifyIcon _notifyIcon;
+        private readonly NotifyIcon _notifyIcon;
+        private readonly Timer _timer;
+
+        private IEventLogger _eventLogger;
+        private ISettingsManager _settingsManager;
 
         public Notificator(IEventLogger eventLogger, ISettingsManager settingsManager)
         {
             _eventLogger = eventLogger;
             _settingsManager = settingsManager;
-            _notifyIcon = new NotifyIcon();
-            _notifyIcon.Icon = new System.Drawing.Icon("TryIcon.ico");
-            _notifyIcon.Visible = true;
+            _notifyIcon = new NotifyIcon
+            {
+                Icon = new Icon("TryIcon.ico"),
+                Visible = true,
+                BalloonTipTitle = "Caution"
+            };
+            _timer = new Timer(60000)
+            {
+                AutoReset = true,
+                Enabled = true
+            };
+            _timer.Elapsed += _timer_Elapsed;
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var settings = _settingsManager.LoadSettings();
+            _notifyIcon.BalloonTipText = string.Format("You will be logged out in {0} minutes", settings.TicksLeft);
+            _notifyIcon.Text = string.Format("{0}/{1} minutes left", settings.TicksLeft, settings.DayTicksLimit);
+            settings.TicksLeft--;
+            _settingsManager.SaveSettings(settings);
+            _notifyIcon.ShowBalloonTip(5000);
         }
     }
 }
