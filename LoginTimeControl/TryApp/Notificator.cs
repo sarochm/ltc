3cdd6350eea418b6f53462b7bdd3e96eb822b26e
Miroslav Saroch <saroch@seznam.cz>
2015-11-09 17:41:05 +0100

    resources not working

--- LoginTimeControl/TryApp/Notificator.cs
+++ LoginTimeControl/TryApp/Notificator.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Drawing;
+using System.Globalization;
+using System.Threading;
 using System.Timers;
 using System.Windows.Forms;
 using Common;
+using TryApp.Properties;
 using Timer = System.Timers.Timer;
 
 namespace TryApp
@@ -17,18 +20,19 @@ namespace TryApp
 
         public Notificator(IEventLogger eventLogger, ISettingsManager settingsManager)
         {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("cs-CZ");
             _eventLogger = eventLogger;
             _settingsManager = settingsManager;
 
-            var menuItem = new MenuItem("Exit");
+            var menuItem = new MenuItem(Strings.Exit);
             menuItem.Click += menuItem_Click;
             var contextMenu = new ContextMenu(new[] {menuItem});
             _notifyIcon = new NotifyIcon
             {
                 Icon = new Icon("TryIcon.ico"),
                 Visible = true,
-                BalloonTipTitle = "Caution",
-                Text = "Initializing",
+                BalloonTipTitle = Strings.Caution,
+                Text = Strings.Initializing,
                 ContextMenu = contextMenu
             };
             _timer = new Timer(60000)
@@ -48,10 +52,10 @@ namespace TryApp
         private void _timer_Elapsed(object sender, ElapsedEventArgs e)
         {
             var settings = _settingsManager.LoadSettings();
-            _notifyIcon.Text = string.Format("{0}/{1} minutes left", settings.TicksLeft, settings.DayTicksLimit);
+            _notifyIcon.Text = string.Format(Strings.MinutesLeft, settings.TicksLeft, settings.DayTicksLimit);
             if (settings.TicksLeft < 6)
             {
-                _notifyIcon.BalloonTipText = string.Format("You will be logged out in {0} minutes", settings.TicksLeft);
+                _notifyIcon.BalloonTipText = string.Format(Strings.YouWillBeLoggedOutIn0Minutes, settings.TicksLeft);
                 _notifyIcon.ShowBalloonTip(3000);
             }
             //settings.TicksLeft--;
--- LoginTimeControl/TryApp/Program.cs
+++ LoginTimeControl/TryApp/Program.cs
@@ -1,8 +1,10 @@
 ﻿using Common;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,6 +21,8 @@ namespace TryApp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("cs-CZ");
 
             var eventLogger = new EventLogger("LTC - TryApp");
             var notificator = new Notificator(eventLogger,new SettingsManager(eventLogger));
