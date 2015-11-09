28d2529f82396b01e4a093c76dce52eb611e6b3a
Miroslav Saroch <saroch@seznam.cz>
2015-11-09 17:53:20 +0100

    Revert "bad revert"
    This reverts commit fb4d93eceaf94de61c2607c2f4bff713b2bfa6d3.

--- LoginTimeControl/LtcWinConfig/MainForm.cs
+++ LoginTimeControl/LtcWinConfig/MainForm.cs
@@ -3,18 +3,57 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Common;
 
 namespace LtcWinConfig
 {
     public partial class MainForm : Form
     {
+        SettingsManager _settingsManager;
+        private Settings _settings;
+
+
         public MainForm()
         {
             InitializeComponent();
+            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            EventLogger eventLogger = new EventLogger("LTC Config");
+            _settingsManager = new SettingsManager(eventLogger);
+            LoadSettings();
+        }
+
+        private void LoadSettings()
+        {
+            _settings = _settingsManager.LoadSettings();
+            numericUpDownDayLimit.Value = _settings.DayTicksLimit;
+            numericUpDownLeft.Value = _settings.TicksLeft;
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            SaveSettings();
+            Application.Exit();
+        }
+
+        private void SaveSettings()
+        {
+            _settings.DayTicksLimit = Convert.ToInt32(numericUpDownDayLimit.Value);
+            _settings.TicksLeft = Convert.ToInt32(numericUpDownLeft.Value);
+            _settingsManager.SaveSettings(_settings);
         }
     }
 }
--- LoginTimeControl/TryApp/Notificator.cs
+++ LoginTimeControl/TryApp/Notificator.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Drawing;
+using System.Globalization;
+using System.Threading;
 using System.Timers;
 using System.Windows.Forms;
 using Common;
+using TryApp.Properties;
 using Timer = System.Timers.Timer;
 
 namespace TryApp
@@ -10,24 +13,28 @@ namespace TryApp
     public class Notificator
     {
         private readonly NotifyIcon _notifyIcon;
+        private readonly ISettingsManager _settingsManager;
         private readonly Timer _timer;
 
         private IEventLogger _eventLogger;
-        private readonly ISettingsManager _settingsManager;
 
         public Notificator(IEventLogger eventLogger, ISettingsManager settingsManager)
         {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("cs-CZ");
             _eventLogger = eventLogger;
             _settingsManager = settingsManager;
-            var contextMenu = new ContextMenu(new[] {new MenuItem("text")});
+
+            var menuItem = new MenuItem(Strings.Exit);
+            menuItem.Click += menuItem_Click;
+            var contextMenu = new ContextMenu(new[] {menuItem});
             _notifyIcon = new NotifyIcon
             {
                 Icon = new Icon("TryIcon.ico"),
                 Visible = true,
-                BalloonTipTitle = "Caution",
+                BalloonTipTitle = Strings.Caution,
+                Text = Strings.Initializing,
                 ContextMenu = contextMenu
             };
-            _notifyIcon.Click += _notifyIcon_Click;
             _timer = new Timer(60000)
             {
                 AutoReset = true,
@@ -36,30 +43,20 @@ namespace TryApp
             _timer.Elapsed += _timer_Elapsed;
         }
 
-        private void _notifyIcon_Click(object sender, EventArgs e)
+        private void menuItem_Click(object sender, EventArgs e)
         {
-            var settings = _settingsManager.LoadSettings();
-            var form = new ChangeSettingsForm(settings.DayTicksLimit, settings.TicksLeft);
-            if (form.ShowDialog() == DialogResult.OK)
-            {
-                settings.TicksLeft = form.TodayLeft();
-                settings.DayTicksLimit = form.DayLimit();
-                if (_settingsManager.SaveSettings(settings) == false)
-                {
-                    MessageBox.Show("You can't write settings.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);                    
-                }
-            }
-            else  Application.Exit();
+            Application.Exit();
         }
+
 
         private void _timer_Elapsed(object sender, ElapsedEventArgs e)
         {
             var settings = _settingsManager.LoadSettings();
-            _notifyIcon.Text = string.Format("{0}/{1} minutes left", settings.TicksLeft, settings.DayTicksLimit);
+            _notifyIcon.Text = string.Format(Strings.MinutesLeft, settings.TicksLeft, settings.DayTicksLimit);
             if (settings.TicksLeft < 6)
             {
-                _notifyIcon.BalloonTipText = string.Format("You will be logged out in {0} minutes", settings.TicksLeft);
-                _notifyIcon.ShowBalloonTip(5000);
+                _notifyIcon.BalloonTipText = string.Format(Strings.YouWillBeLoggedOutIn0Minutes, settings.TicksLeft);
+                _notifyIcon.ShowBalloonTip(3000);
             }
             //settings.TicksLeft--;
             //_settingsManager.SaveSettings(settings);
