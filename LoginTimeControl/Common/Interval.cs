4b77e8f2fe0033d9bc4a9dbe2046c5b55a9376cb
Miroslav Saroch <saroch@seznam.cz>
2015-12-03 20:26:45 +0100

    added iterval serializable class

---
+++ LoginTimeControl/Common/Interval.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Common
+{
+    [Serializable]
+    public class Interval
+    {
+        private const string SerializingTimeFormat = @"hh\:mm";
+
+        [XmlIgnore]
+        public TimeSpan TimeFrom { get; set; }
+
+        [XmlIgnore]
+        public TimeSpan TimeTo { get; set; }
+
+        [XmlElement(ElementName = "TimeFrom")]
+        public string TimeFromStr
+        {
+            get { return TimeFrom.ToString(SerializingTimeFormat); }
+            set { TimeFrom = TimeSpan.ParseExact(value, SerializingTimeFormat, null); }
+        }
+
+        [XmlElement(ElementName = "TimeTo")]
+        public string TimeToStr
+        {
+            get { return TimeTo.ToString(SerializingTimeFormat); }
+            set { TimeTo = TimeSpan.ParseExact(value, SerializingTimeFormat, null); }
+        }
+    }
+}
