43f71fd8039ce820db3f803456bb5530d50c61f1
Miroslav Saroch <saroch@seznam.cz>
2015-12-24 10:09:34 +0100

    main window design

--- LoginTimeControl/LtcWinConfig/MainForm.Designer.cs
+++ LoginTimeControl/LtcWinConfig/MainForm.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.labelLeft = new System.Windows.Forms.Label();
             this.numericUpDownLeft = new System.Windows.Forms.NumericUpDown();
-            this.labelDayLimits = new System.Windows.Forms.Label();
             this.numericUpDownSunday = new System.Windows.Forms.NumericUpDown();
             this.buttonCancel = new System.Windows.Forms.Button();
             this.buttonOK = new System.Windows.Forms.Button();
@@ -50,6 +49,8 @@
             this.listBoxAllowedIntervals = new System.Windows.Forms.ListBox();
             this.buttonRemove = new System.Windows.Forms.Button();
             this.buttonAdd = new System.Windows.Forms.Button();
+            this.groupBoxIntervals = new System.Windows.Forms.GroupBox();
+            this.groupBoxDayLimits = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLeft)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSunday)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMonday)).BeginInit();
@@ -58,13 +59,15 @@
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownThursday)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownFriday)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSaturday)).BeginInit();
+            this.groupBoxIntervals.SuspendLayout();
+            this.groupBoxDayLimits.SuspendLayout();
             this.SuspendLayout();
             // 
             // labelLeft
             // 
             this.labelLeft.AutoSize = true;
             this.labelLeft.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.labelLeft.Location = new System.Drawing.Point(12, 102);
+            this.labelLeft.Location = new System.Drawing.Point(12, 287);
             this.labelLeft.Name = "labelLeft";
             this.labelLeft.Size = new System.Drawing.Size(93, 13);
             this.labelLeft.TabIndex = 12;
@@ -72,14 +75,14 @@
             // 
             // numericUpDownLeft
             // 
-            this.numericUpDownLeft.Location = new System.Drawing.Point(111, 100);
+            this.numericUpDownLeft.Location = new System.Drawing.Point(111, 285);
             this.numericUpDownLeft.Maximum = new decimal(new int[] {
             1000,
             0,
             0,
             0});
             this.numericUpDownLeft.Name = "numericUpDownLeft";
-            this.numericUpDownLeft.Size = new System.Drawing.Size(75, 20);
+            this.numericUpDownLeft.Size = new System.Drawing.Size(72, 20);
             this.numericUpDownLeft.TabIndex = 11;
             this.numericUpDownLeft.Value = new decimal(new int[] {
             3,
@@ -87,19 +90,9 @@
             0,
             0});
             // 
-            // labelDayLimits
-            // 
-            this.labelDayLimits.AutoSize = true;
-            this.labelDayLimits.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.labelDayLimits.Location = new System.Drawing.Point(12, 9);
-            this.labelDayLimits.Name = "labelDayLimits";
-            this.labelDayLimits.Size = new System.Drawing.Size(101, 13);
-            this.labelDayLimits.TabIndex = 10;
-            this.labelDayLimits.Text = "Day limits in minutes";
-            // 
             // numericUpDownSunday
             // 
-            this.numericUpDownSunday.Location = new System.Drawing.Point(15, 54);
+            this.numericUpDownSunday.Location = new System.Drawing.Point(9, 34);
             this.numericUpDownSunday.Maximum = new decimal(new int[] {
             999,
             0,
@@ -119,7 +112,7 @@
             // 
             this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.buttonCancel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.buttonCancel.Location = new System.Drawing.Point(108, 135);
+            this.buttonCancel.Location = new System.Drawing.Point(108, 320);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(75, 23);
             this.buttonCancel.TabIndex = 8;
@@ -131,7 +124,7 @@
             // 
             this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.buttonOK.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.buttonOK.Location = new System.Drawing.Point(27, 135);
+            this.buttonOK.Location = new System.Drawing.Point(15, 320);
             this.buttonOK.Name = "buttonOK";
             this.buttonOK.Size = new System.Drawing.Size(75, 23);
             this.buttonOK.TabIndex = 7;
@@ -143,7 +136,7 @@
             // 
             this.labelSunday.AutoSize = true;
             this.labelSunday.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.labelSunday.Location = new System.Drawing.Point(12, 38);
+            this.labelSunday.Location = new System.Drawing.Point(6, 18);
             this.labelSunday.Name = "labelSunday";
             this.labelSunday.Size = new System.Drawing.Size(43, 13);
             this.labelSunday.TabIndex = 13;
@@ -153,7 +146,7 @@
             // 
             this.labelMonday.AutoSize = true;
             this.labelMonday.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.labelMonday.Location = new System.Drawing.Point(58, 38);
+            this.labelMonday.Location = new System.Drawing.Point(52, 18);
             this.labelMonday.Name = "labelMonday";
             this.labelMonday.Size = new System.Drawing.Size(45, 13);
             this.labelMonday.TabIndex = 15;
@@ -161,7 +154,7 @@
             // 
             // numericUpDownMonday
             // 
-            this.numericUpDownMonday.Location = new System.Drawing.Point(61, 54);
+            this.numericUpDownMonday.Location = new System.Drawing.Point(55, 34);
             this.numericUpDownMonday.Maximum = new decimal(new int[] {
             999,
             0,
@@ -181,7 +174,7 @@
             // 
             this.labelTuesday.AutoSize = true;
             this.labelTuesday.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.labelTuesday.Location = new System.Drawing.Point(104, 38);
+            this.labelTuesday.Location = new System.Drawing.Point(98, 18);
             this.labelTuesday.Name = "labelTuesday";
             this.labelTuesday.Size = new System.Drawing.Size(48, 13);
             this.labelTuesday.TabIndex = 17;
@@ -189,7 +182,7 @@
             // 
             // numericUpDownTuesday
             // 
-            this.numericUpDownTuesday.Location = new System.Drawing.Point(107, 54);
+            this.numericUpDownTuesday.Location = new System.Drawing.Point(101, 34);
             this.numericUpDownTuesday.Maximum = new decimal(new int[] {
             999,
             0,
@@ -209,7 +202,7 @@
             // 
             this.labelWednesday.AutoSize = true;
             this.labelWednesday.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.labelWednesday.Location = new System.Drawing.Point(150, 38);
+            this.labelWednesday.Location = new System.Drawing.Point(144, 18);
             this.labelWednesday.Name = "labelWednesday";
             this.labelWednesday.Size = new System.Drawing.Size(64, 13);
             this.labelWednesday.TabIndex = 19;
@@ -217,7 +210,7 @@
             // 
             // numericUpDownWednesday
             // 
-            this.numericUpDownWednesday.Location = new System.Drawing.Point(153, 54);
+            this.numericUpDownWednesday.Location = new System.Drawing.Point(147, 34);
             this.numericUpDownWednesday.Maximum = new decimal(new int[] {
             999,
             0,
@@ -237,7 +230,7 @@
             // 
             this.labelThursday.AutoSize = true;
             this.labelThursday.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.labelThursday.Location = new System.Drawing.Point(196, 38);
+            this.labelThursday.Location = new System.Drawing.Point(190, 18);
             this.labelThursday.Name = "labelThursday";
             this.labelThursday.Size = new System.Drawing.Size(51, 13);
             this.labelThursday.TabIndex = 21;
@@ -245,7 +238,7 @@
             // 
             // numericUpDownThursday
             // 
-            this.numericUpDownThursday.Location = new System.Drawing.Point(199, 54);
+            this.numericUpDownThursday.Location = new System.Drawing.Point(193, 34);
             this.numericUpDownThursday.Maximum = new decimal(new int[] {
             999,
             0,
@@ -265,7 +258,7 @@
             // 
             this.labelFriday.AutoSize = true;
             this.labelFriday.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.labelFriday.Location = new System.Drawing.Point(242, 38);
+            this.labelFriday.Location = new System.Drawing.Point(236, 18);
             this.labelFriday.Name = "labelFriday";
             this.labelFriday.Size = new System.Drawing.Size(35, 13);
             this.labelFriday.TabIndex = 23;
@@ -273,7 +266,7 @@
             // 
             // numericUpDownFriday
             // 
-            this.numericUpDownFriday.Location = new System.Drawing.Point(245, 54);
+            this.numericUpDownFriday.Location = new System.Drawing.Point(239, 34);
             this.numericUpDownFriday.Maximum = new decimal(new int[] {
             999,
             0,
@@ -293,7 +286,7 @@
             // 
             this.labelSaturday.AutoSize = true;
             this.labelSaturday.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.labelSaturday.Location = new System.Drawing.Point(288, 38);
+            this.labelSaturday.Location = new System.Drawing.Point(282, 18);
             this.labelSaturday.Name = "labelSaturday";
             this.labelSaturday.Size = new System.Drawing.Size(49, 13);
             this.labelSaturday.TabIndex = 25;
@@ -301,7 +294,7 @@
             // 
             // numericUpDownSaturday
             // 
-            this.numericUpDownSaturday.Location = new System.Drawing.Point(291, 54);
+            this.numericUpDownSaturday.Location = new System.Drawing.Point(285, 34);
             this.numericUpDownSaturday.Maximum = new decimal(new int[] {
             999,
             0,
@@ -320,15 +313,15 @@
             // listBoxAllowedIntervals
             // 
             this.listBoxAllowedIntervals.FormattingEnabled = true;
-            this.listBoxAllowedIntervals.Location = new System.Drawing.Point(15, 184);
+            this.listBoxAllowedIntervals.Location = new System.Drawing.Point(6, 17);
             this.listBoxAllowedIntervals.Name = "listBoxAllowedIntervals";
-            this.listBoxAllowedIntervals.Size = new System.Drawing.Size(340, 121);
+            this.listBoxAllowedIntervals.Size = new System.Drawing.Size(368, 121);
             this.listBoxAllowedIntervals.TabIndex = 26;
             this.listBoxAllowedIntervals.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBoxAllowedIntervals_MouseDoubleClick);
             // 
             // buttonRemove
             // 
-            this.buttonRemove.Location = new System.Drawing.Point(15, 312);
+            this.buttonRemove.Location = new System.Drawing.Point(6, 142);
             this.buttonRemove.Name = "buttonRemove";
             this.buttonRemove.Size = new System.Drawing.Size(75, 23);
             this.buttonRemove.TabIndex = 27;
@@ -338,7 +331,7 @@
             // 
             // buttonAdd
             // 
-            this.buttonAdd.Location = new System.Drawing.Point(97, 312);
+            this.buttonAdd.Location = new System.Drawing.Point(84, 142);
             this.buttonAdd.Name = "buttonAdd";
             this.buttonAdd.Size = new System.Drawing.Size(75, 23);
             this.buttonAdd.TabIndex = 28;
@@ -346,33 +339,53 @@
             this.buttonAdd.UseVisualStyleBackColor = true;
             this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
             // 
+            // groupBoxIntervals
+            // 
+            this.groupBoxIntervals.Controls.Add(this.listBoxAllowedIntervals);
+            this.groupBoxIntervals.Controls.Add(this.buttonAdd);
+            this.groupBoxIntervals.Controls.Add(this.buttonRemove);
+            this.groupBoxIntervals.Location = new System.Drawing.Point(4, 12);
+            this.groupBoxIntervals.Name = "groupBoxIntervals";
+            this.groupBoxIntervals.Size = new System.Drawing.Size(380, 170);
+            this.groupBoxIntervals.TabIndex = 29;
+            this.groupBoxIntervals.TabStop = false;
+            this.groupBoxIntervals.Text = "Allowed intervals";
+            // 
+            // groupBoxDayLimits
+            // 
+            this.groupBoxDayLimits.Controls.Add(this.numericUpDownTuesday);
+            this.groupBoxDayLimits.Controls.Add(this.labelSaturday);
+            this.groupBoxDayLimits.Controls.Add(this.numericUpDownSunday);
+            this.groupBoxDayLimits.Controls.Add(this.numericUpDownSaturday);
+            this.groupBoxDayLimits.Controls.Add(this.labelSunday);
+            this.groupBoxDayLimits.Controls.Add(this.labelFriday);
+            this.groupBoxDayLimits.Controls.Add(this.numericUpDownMonday);
+            this.groupBoxDayLimits.Controls.Add(this.numericUpDownFriday);
+            this.groupBoxDayLimits.Controls.Add(this.labelMonday);
+            this.groupBoxDayLimits.Controls.Add(this.labelThursday);
+            this.groupBoxDayLimits.Controls.Add(this.labelTuesday);
+            this.groupBoxDayLimits.Controls.Add(this.numericUpDownThursday);
+            this.groupBoxDayLimits.Controls.Add(this.numericUpDownWednesday);
+            this.groupBoxDayLimits.Controls.Add(this.labelWednesday);
+            this.groupBoxDayLimits.Location = new System.Drawing.Point(4, 193);
+            this.groupBoxDayLimits.Name = "groupBoxDayLimits";
+            this.groupBoxDayLimits.Size = new System.Drawing.Size(380, 65);
+            this.groupBoxDayLimits.TabIndex = 30;
+            this.groupBoxDayLimits.TabStop = false;
+            this.groupBoxDayLimits.Text = "Day limits in minutes";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(367, 356);
-            this.Controls.Add(this.buttonAdd);
-            this.Controls.Add(this.buttonRemove);
-            this.Controls.Add(this.listBoxAllowedIntervals);
-            this.Controls.Add(this.labelSaturday);
-            this.Controls.Add(this.numericUpDownSaturday);
-            this.Controls.Add(this.labelFriday);
-            this.Controls.Add(this.numericUpDownFriday);
-            this.Controls.Add(this.labelThursday);
-            this.Controls.Add(this.numericUpDownThursday);
-            this.Controls.Add(this.labelWednesday);
-            this.Controls.Add(this.numericUpDownWednesday);
-            this.Controls.Add(this.labelTuesday);
-            this.Controls.Add(this.numericUpDownTuesday);
-            this.Controls.Add(this.labelMonday);
-            this.Controls.Add(this.numericUpDownMonday);
-            this.Controls.Add(this.labelSunday);
+            this.ClientSize = new System.Drawing.Size(387, 356);
+            this.Controls.Add(this.groupBoxDayLimits);
+            this.Controls.Add(this.groupBoxIntervals);
             this.Controls.Add(this.labelLeft);
             this.Controls.Add(this.numericUpDownLeft);
-            this.Controls.Add(this.labelDayLimits);
-            this.Controls.Add(this.numericUpDownSunday);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MainForm";
@@ -387,6 +400,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownThursday)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownFriday)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSaturday)).EndInit();
+            this.groupBoxIntervals.ResumeLayout(false);
+            this.groupBoxDayLimits.ResumeLayout(false);
+            this.groupBoxDayLimits.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -396,7 +412,6 @@
 
         private System.Windows.Forms.Label labelLeft;
         private System.Windows.Forms.NumericUpDown numericUpDownLeft;
-        private System.Windows.Forms.Label labelDayLimits;
         private System.Windows.Forms.NumericUpDown numericUpDownSunday;
         private System.Windows.Forms.Button buttonCancel;
         private System.Windows.Forms.Button buttonOK;
@@ -416,6 +431,8 @@
         private System.Windows.Forms.ListBox listBoxAllowedIntervals;
         private System.Windows.Forms.Button buttonRemove;
         private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.GroupBox groupBoxIntervals;
+        private System.Windows.Forms.GroupBox groupBoxDayLimits;
     }
 }
 
