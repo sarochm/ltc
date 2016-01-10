cc883a10ceb311dad9e9a3c697c07565af9e6ea7
Miroslav Saroch <saroch@seznam.cz>
2016-01-10 17:05:59 +0100

    TryApp can run only once

--- LoginTimeControl/TryApp/Program.cs
+++ LoginTimeControl/TryApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Globalization;
 using System.IO;
+using System.Threading;
 using System.Windows.Forms;
 using Common;
 
@@ -14,15 +14,23 @@ namespace TryApp
         [STAThread]
         private static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
-            //CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.GetCultureInfo("cs-CZ");
+            var createdNew = true;
+            using (var mutex = new Mutex(true, "MSC-LTC-TryApp", out createdNew))
+            // for only one instance http://stackoverflow.com/questions/184084/how-to-force-c-sharp-net-app-to-run-only-one-instance-in-windows
+            {
+                if (createdNew)
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+                    //CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.GetCultureInfo("cs-CZ");
 
-            var eventLogger = new EventLogger("LTC - TryApp");
-            var notificator = new Notificator(eventLogger, new SettingsManager(eventLogger));
+                    var eventLogger = new EventLogger("LTC - TryApp");
+                    var notificator = new Notificator(eventLogger, new SettingsManager(eventLogger));
 
-            Application.Run();
+                    Application.Run();
+                }
+            }
         }
     }
 }
